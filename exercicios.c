e8530d4d06fd6ec3f65b37d2fcbac74219916f36
010010110 <42617810+010010110@users.noreply.github.com>
2018-08-24 13:01:08 -0300

    Update exercicios.c

--- exercicios.c
+++ exercicios.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+//void linha();
+void num01();
 
-return 0;
+int main(){
+  int exercicio;
+  //linha();
+  printf("Qual o numero do exercício? \n"); 
+  scanf("%d", &exercicio);
+  //linha();
+  
+  switch(exercicio){
+    case (1):
+      printf("Exercicío 1: \n");
+      void num01();
+    break;
+    default:
+            printf("Exercicio não encontrado ou não terminado :) \n");
+    break;      
+  }
+}  
+
+/*void linha(){
+      printf("----------------------------------------------------\n");
+      linha();
+  }*/
+  
+
+             
+void num01(){
+        float num1, num2, num3;
+        printf("Informe os numero 1, numero 2 e o numero 3");
+        scanf("%f %f %f", &num1, &num2, &num3);
+        if (num1 >=0)
+          printf("Valor é POSITIVO: %f", sqrt(num1));
+        else  
+          printf("O valor é NEGATIVO: %f", pow(num1,4));
+      //linha();
 }
